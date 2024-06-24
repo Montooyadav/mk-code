4a19e4ec64072fac0e81d76fffeaf2c24e58710a
Montooyadav <kumarmontoo2507@gmail.com>
2024-06-24 12:56:05 +0530

    okk

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 23344
+ accountEmail = "raghav1297@gmail.com"
+ accountPassword = "1234"
+accountCity = "bhilwara"
+accountkey = "okk"
+
+
+
+console.table([accountId,accountPassword,accountEmail,accountCity,accountkey])
+console.log(2+2);
